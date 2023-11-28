191c12a8e6e8f1693bd2e4a8a33dc1e14401c3e8
nataberger <natasha.berger@innew.la>
2023-11-28 09:54:52 -0300

    integración | inicializado node | instalación de express + creación de rutas estáticas

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3002;
+
+app.use(express.static('public'));
+app.use(express.static('public/pages'));
+
+
+// app.use((req, res) => {
+//     res.status(404).send("Recurso no encontrado")
+// })
+app.get('shop', (req, res) => {
+    res.sendFile(__direname + '/public/pages/shop.html')
+
+})
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/pages/index.html')
+})
+app.get('login', (req, res) => {
+    res.sendFile(__dirname + '/public/pages/login.html')
+})
+app.get('register', (req, res) => {
+    res.sendFile(__dirname + '/public/pages/register.html')
+})
+app.get('item', (req, res) => {
+    res.sendFile(__dirname + '/public/pages/item.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor inicializado en http://localhost:${PORT}`)
+})
