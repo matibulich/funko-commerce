f48567b9a6c2cdb3f97fa5c86841f1b0c90254a7
matibulich <>
2023-12-06 22:53:27 -0300

    prueba rutas

---
+++ src/routes/main_routes.js
@@ -0,0 +1,13 @@
+// rutas del main: index, login, register, etc
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '../index.html'))
+})
+
+
+
+
+module.exports = router;
