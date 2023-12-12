88f98cb5c3a956018a83ad15501e380f4926b5a3
HernanSasdelli <her9192copia@gmail.com>
2023-12-12 03:42:28 -0300

    cambiada navbar rutas funcionando

--- src/controllers/main_controller.js
+++ src/controllers/main_controller.js
@@ -1,9 +1,9 @@
 // controlador y coso
 
 module.exports= {
-    home: (req, res) => { res.render('index',{title: "HOME"});  },
+    home: (req, res) => { res.render('index',{title: "Home"});  },
     
-    contacto:  (req, res) => { res.render('contacto');  }
+    contacto:  (req, res) => { res.render('contacto',{title: "Contacto"});  }
 
 
 }
--- src/controllers/shop_controller.js
+++ src/controllers/shop_controller.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 module.exports = { 
     shop: (req, res) => {
-         res.render(path.resolve(__dirname,'../views/shop/shop.ejs'))},
+         res.render(path.resolve(__dirname,'../views/shop/shop.ejs'));},
+         /*res.render(path.resolve(__dirname,'../views/shop/shop.ejs'),{title: "Shop"});},*/ //ver con profe
 
 
     item: (req, res) => {
