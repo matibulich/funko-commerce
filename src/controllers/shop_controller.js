488ed1b7147a483e175d5a207a3b571f3f37a9bb
HernanSasdelli <her9192copia@gmail.com>
2023-12-12 16:51:15 -0300

    links andando

--- src/controllers/shop_controller.js
+++ src/controllers/shop_controller.js
@@ -13,7 +13,7 @@ module.exports = {
     
     addItem: (req, res) => res.send('Nuevo item'),
 
-    carriro: (req, res) => {
+    carrito: (req, res) => {
         res.render(path.resolve(__dirname,'../views/shop/carrito.ejs'))},
 
     addCarrito: (req, res) => res.send('agregar item'),
--- src/routes/shop_routes.js
+++ src/routes/shop_routes.js
@@ -6,7 +6,7 @@ const controller = require('../controllers/shop_controller');
 router.get('/', controller.shop);
 router.get('/item/:id', controller.item);
 router.post('/item:id/add', controller.addItem);
-/*router.get('/carrito', controller.carrito);
-router.post('/carrito', controller.addCarrito);*/
+router.get('/carrito', controller.carrito);
+router.post('/carrito', controller.addCarrito);
 
 module.exports = router;
