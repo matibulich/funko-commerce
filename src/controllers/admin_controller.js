75f896d53eac96b369155d0a84c69170b84cba17
matibulich <>
2023-12-15 12:36:49 -0300

    listado de productos en admin, correccion de item/img

--- src/controllers/admin_controller.js
+++ src/controllers/admin_controller.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const articulos = require('../data/articulos.json')
 
 
 
 module.exports = { 
-    admin: (req, res) => res.render('admin/admin',{title: "Admin", role: "admin"}),
+    admin: (req, res) => res.render('admin/admin',{title: "Admin", role: "admin", articulos}),
     create: (req, res) => res.render('admin/create',{title: "Create", role: "admin"}),       
     edit: (req, res) => res.render('admin/edit',{title: "Edit", role: "admin"}),
 
