37cdd5264a1b7a6c74cade23fd86d5fe46aae48a
matibulich <>
2023-12-16 20:15:21 -0300

    muestra edit.ejs con los datos del JSON

--- src/controllers/admin_controller.js
+++ src/controllers/admin_controller.js
@@ -6,7 +6,20 @@ const articulos = require('../data/articulos.json')
 module.exports = { 
     admin: (req, res) => res.render('admin/admin',{title: "Admin", role: "admin", articulos}),
     create: (req, res) => res.render('admin/create',{title: "Create", role: "admin"}),       
-    edit: (req, res) => res.render('admin/edit',{title: "Edit", role: "admin"}),
+    edit: (req, res) => {
+               
+        
+        const item_id = Number(req.params .id);
+        const item = articulos.find(articulo => articulo.id === item_id )
+        
+        
+        if (item) {
+            res.render('admin/edit', { title: "Item.id", item ,articulos});//agregar un parametro para que lo renderise tambien!!
+        } else {
+            res.status(404).send('Item no encotrado');
+        }
+        
+    },
 
 
     editItem: (req, res) => res.send('impacta la modificacion'),
