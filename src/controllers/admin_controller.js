cc63e5a4b2926d2df18270a4c69c0e7e6e2fa75d
HernanSasdelli <her9192copia@gmail.com>
2023-12-18 14:02:35 -0300

    creando el edit

--- src/controllers/admin_controller.js
+++ src/controllers/admin_controller.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const articulos = require('../data/articulos.json')
-
+const fs = require('fs');
 
 
 module.exports = { 
     admin: (req, res) => res.render('admin/admin',{title: "Admin", role: "admin", articulos}),
-    create: (req, res) => res.render('admin/create',{title: "Create", role: "admin"}),       
-    edit: (req, res) => {
-               
+    create: (req, res) => res.render('admin/create',{title: "Create", role: "admin"}),    
+
+    edit: (req, res) => {               
         
         const item_id = Number(req.params .id);
         const item = articulos.find(articulo => articulo.id === item_id )
@@ -17,13 +17,28 @@ module.exports = {
             res.render('admin/edit', { title: "Item.id", item ,articulos});//agregar un parametro para que lo renderise tambien!!
         } else {
             res.status(404).send('Item no encotrado');
-        }
-        
+        }        
+    },
+
+    editAction: (req, res) => {
+        const item_id = Number(req.params.id);
+        const data = fs.readFileSync(articulos, 'utf-8');
+        const jsonData = JSON.parse(data);
+        const unItem = jsonData.find(item => item.id === item_id);//encuentro el item del id adentro del json
+
+        if (unItem) {
+            res.json(unItem);
+          } else {
+            res.status(404).json({ error: 'Objeto no encontrado' });
+          }
+
     },
 
 
     editItem: (req, res) => res.send('impacta la modificacion'),
+
     createItem: (req, res) => res.send('agregar'),
+
     deleteItem: (req, res) => {res.render(path.resolve(__dirname,'../views/admin/edit.ejs'))},
     
         
--- src/routes/admin_routes.js
+++ src/routes/admin_routes.js
@@ -12,6 +12,9 @@ router.get('/create',  create)//devuelve html vista del create
 router.post('/create',  createItem)//ruta para agregar nuevo item, no devuelve vista, crea un nuevo item en la base
 router.get('/edit', (req, res) => res.redirect("/"))
 router.get('/edit/:id', edit)//editar item especifico
+//router.post('/edit/:id', editAction)//post en la misma ruta lo devuelve al edit
+
+
 
 router.put('/edit/:id',  editItem)//recupero elemento para edita o eliminar
 router.delete('/delete/:id', deleteItem)//eliminar
