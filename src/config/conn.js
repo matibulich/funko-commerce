2e17d83e12800c342e2d80358bb62aae56817e00
matibulich <>
2024-06-01 15:29:26 -0300

    solucion views

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,16 +1,17 @@
 //IMPORT LIBRERIA MYSQL2
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "funko_db",
-  password: "matias123",
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DB,
+  password: process.env.DBPASS,
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0, //queries en cola infinitas
-});
+});   	
 
 //PRUEBA DE CONEXION
 pool.getConnection((error, concect) => {
